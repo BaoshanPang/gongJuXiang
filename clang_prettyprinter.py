e3108af6b5cc2133697479850c06f46c207682fa
Baoshan Pang <pangbw@gmail.com>
2021-10-11 20:07:35 -0700

    add printer for MultiLevelTemplateArgumentList
    The result is weired, not sure how to fix it so far

--- clang_prettyprinter.py
+++ clang_prettyprinter.py
@@ -17,7 +17,31 @@ class SourceLocationPrinter:
         eval_string = "(*("+str(self.val.type)+"*)("+str(self.val.address)+")).dump(this->getSourceManager())"
         return gdb.parse_and_eval(eval_string);
 
+class MultiLevelTemplateArgumentListPrinter:
+    "MultiLevelTemplateArgumentList"
+
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+       eval_string = "(*("+str(self.val.type)+"*)("+str(self.val.address)+")).TemplateArgumentLists.size()"
+       depth = int(gdb.parse_and_eval(eval_string))
+       strstr = "\n"
+       for i in range(depth):
+           eval_string = "(*("+str(self.val.type)+"*)("+str(self.val.address)+")).TemplateArgumentLists[{}].size()".format(i)
+           sz = int(gdb.parse_and_eval(eval_string))
+           strstr += "level: {} ".format(i)
+           for j in range(sz):
+               strstr += "index {} ".format(j)
+               eval_string = "(*("+str(self.val.type)+"*)("+str(self.val.address)+")).TemplateArgumentLists[{}][{}].dump()".format(i,j)
+               strstr += "\n" + str(gdb.parse_and_eval(eval_string))
+       return  strstr
 
 pp = gdb.printing.RegexpCollectionPrettyPrinter("CLANGSupport")
-pp.add_printer('SourceLocationPrinter', '^clang::SourceLocation$', SourceLocationPrinter)
+pp.add_printer('SourceLocationPrinter',
+               '^clang::SourceLocation$',
+               SourceLocationPrinter)
+pp.add_printer('MultiLevelTemplateArgumentListPrinter',
+               '^clang::MultiLevelTemplateArgumentList$',
+               MultiLevelTemplateArgumentListPrinter)
 gdb.printing.register_pretty_printer(gdb.current_objfile(), pp, True)
